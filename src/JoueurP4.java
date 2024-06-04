64e74e37abe8d421ebede7060b336fb43fa76643
Kylian <kylian.dumas@etu.univ-orleans.fr>
2024-06-04 08:54:21 +0200

    doc joueur

--- src/JoueurP4.java
+++ src/JoueurP4.java
@@ -1,22 +1,37 @@
 public class JoueurP4 {
-
+    /*
+     * Le nom du joueur
+     */
     private String pseudo;
+    /*
+     * une liste de 0 et de 1 qui définissent si un joueur à des skins ou non
+     */
     private String key_skins;
 
+    /*
+     * Constructeur qui crée un nouveau joueur sans skins
+     */
     public JoueurP4(String pseudo){
         this.pseudo = pseudo;
         this.key_skins = "";    
     }
 
+    /*
+     * Un constructeur qui crée un nouveau joueur qui a déjà des skins
+     */
     public JoueurP4(String pseudo, String key_skins){
         this.pseudo = pseudo;
         this.key_skins = key_skins;    
     }
 
+    /*Les getters */
     public String getPseudo(){return this.pseudo;}
 
     public String getSkins(){return this.key_skins;}
-    
+
+    /*
+     * Vérifie si un joueur possède un certain skin à un certain indice, en vérifiant qu'il y a un 1 à l'indice donné
+     */
     public boolean aSkin(int ind){
         int cpt = 0;
         for(char c: getSkins().toCharArray()){
