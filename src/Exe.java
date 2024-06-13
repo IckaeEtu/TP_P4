e1220ad39a0c81ccd63f8cd28ac17a899f5cfc5f
IckaeEtu <mickael.riego@etu.univ-orleans.fr>
2024-06-13 08:06:35 +0200

    fait: victoire diag | a faire: victoire diag/ligne, grille pleine

--- src/Exe.java
+++ src/Exe.java
@@ -1,22 +1,18 @@
 public class Exe {
     public static void main(String[] args) {
         ModeleP4 p = new ModeleP4();
-        p.jouer(6);
-        p.jouer(5);
-        p.jouer(5);
-        p.jouer(4);
-        p.jouer(4); 
-        p.jouer(3);
-        p.jouer(4);
-        p.jouer(3);
+        p.jouer(0);
+        p.jouer(1);
+        p.jouer(0);
+        p.jouer(1);
+        p.jouer(0); 
+        p.jouer(2);
+        p.jouer(0);
         p.jouer(3);
     
-
-        System.out.println(p.victoireDiag2(0,6));
-        System.out.println(p.victoire(2,4));
+        System.out.println(p.victoireColonne(3,0));
         System.out.println(p.convert(p.getJeton(5, 6)));
         System.out.println(p.getJoueurActuel());
-        System.out.println(p.victoire(4,5));
         System.out.println(p.toString());
     }
 }
--- src/ModeleP4.java
+++ src/ModeleP4.java
@@ -13,161 +13,204 @@ public class ModeleP4 {
   private String joueurActuel = "J1";
 
   /**
-   * Initialise le plateau 
+   * Initialise le plateau
    */
-  public ModeleP4(){
+  public ModeleP4() {
     plateau = new ArrayList<>();
-    for (int i=0;i<largeur;i++){
+    for (int i = 0; i < largeur; i++) {
       plateau.add(new ArrayList<>());
-      for (int j=0;j<hauteur;j++){
+      for (int j = 0; j < hauteur; j++) {
         plateau.get(i).add(0);
       }
     }
   }
 
-  //**Permet de poser un jeton dans la colonne donné en paramètre */
-  public void jouer(int colonne){
-    if (!(colPleinne(colonne))){
+  // **Permet de poser un jeton dans la colonne donné en paramètre */
+  public void jouer(int colonne) {
+    if (!(colPleinne(colonne))) {
       int ligne = ligneVide(colonne);
-      if (joueurActuel.equals("J1")){
-        plateau.get(colonne).set(ligne,1);
+      if (joueurActuel.equals("J1")) {
+        plateau.get(colonne).set(ligne, 1);
+      } else {
+        plateau.get(colonne).set(ligne, 2);
       }
-      else {
-        plateau.get(colonne).set(ligne,2);
-      }
-      victoire(ligne,colonne);
+      //victoire(ligne, colonne);
       changerJoueur();
     }
   }
 
-
-  public boolean colPleinne(int colonne){
+  public boolean colPleinne(int colonne) {
     return plateau.get(colonne).size() < hauteur;
   }
-  
-  public int getJeton(int ligne, int colonne){
+
+  public int getJeton(int ligne, int colonne) {
     return plateau.get(colonne).get(ligne);
   }
 
-  public String convert(int num){
-    if(num == 1){
+  public String convert(int num) {
+    if (num == 1) {
       return "J1";
-    } else {return "J2";}
+    } else if (num == 2) {
+      return "J2";
+    }
+    return "Non";
   }
 
-  public void changerJoueur(){
-    if (joueurActuel.equals("J1")){joueurActuel = "J2";}
-    else {joueurActuel = "J1";}
+  public boolean lautreJoueur(String joueur) {
+    if (joueur.equals("J2") && joueurActuel.equals("J1")) {
+      return true;
+    }
+    if (joueur.equals("J1") && joueurActuel.equals("J2")) {
+      return true;
+    }
+    return false;
   }
 
-  public String getJoueurActuel(){
+  public void changerJoueur() {
+    if (joueurActuel.equals("J1")) {
+      joueurActuel = "J2";
+    } else {
+      joueurActuel = "J1";
+    }
+  }
+
+  public String getJoueurActuel() {
     return this.joueurActuel;
   }
 
-  public int ligneVide(int colonne){
-    for (int i=0;i<hauteur;i++){
-      if (plateau.get(colonne).get(i)==0){
+  public int ligneVide(int colonne) {
+    for (int i = 0; i < hauteur; i++) {
+      if (plateau.get(colonne).get(i) == 0) {
         return i;
       }
     }
     return 42;
   }
 
-  public boolean victoire(int ligne, int colonne){
-    return victoireDiag1(ligne, colonne) || victoireDiag2(ligne, colonne) || victoireColonne(ligne, colonne) || victoireLigne(ligne,colonne);
+  public boolean victoire(int ligne, int colonne) {
+    return victoireDiag1(ligne, colonne) || victoireDiag2(ligne, colonne) || victoireColonne(ligne, colonne)
+        || victoireLigne(ligne, colonne);
   }
 
-  public boolean victoireDiag1(int ligne, int colonne){
+  public boolean victoireDiag1(int ligne, int colonne) {
     int parcours_lig = ligne;
     int parcours_col = colonne;
     int cpt = 1;
-    while (parcours_lig < hauteur && parcours_col < largeur && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
+    while (parcours_lig < hauteur && parcours_col < largeur
+        && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))) {
       parcours_lig++;
       parcours_col++;
       cpt++;
-      if (cpt == 4) {return true;}
+      if (cpt == 4) {
+        return true;
+      }
     }
     parcours_lig = ligne;
     parcours_col = colonne;
-    while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col > 0 && parcours_col < largeur && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
-        parcours_lig--;
-        parcours_col--;
-        cpt++;
-        if (cpt == 4) {return true;}
+    while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col > 0 && parcours_col < largeur
+        && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))) {
+      parcours_lig--;
+      parcours_col--;
+      cpt++;
+      if (cpt == 4) {
+        return true;
+      }
     }
-    return false;                                                                 
-  } 
+    return false;
+  }
 
-  public boolean victoireDiag2(int ligne, int colonne){
+  public boolean victoireDiag2(int ligne, int colonne) {
     int parcours_lig = ligne;
     int parcours_col = colonne;
     int cpt = 0;
-    while (parcours_lig >= 0 && parcours_lig < hauteur && parcours_col < largeur && parcours_col > 0 && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
+    while (parcours_lig >= 0 && parcours_lig < hauteur && parcours_col < largeur && parcours_col > 0
+        && lautreJoueur(convert(getJeton(ligne, colonne)))) {
       parcours_lig++;
       parcours_col--;
       cpt++;
-      if (cpt == 4) {return true;}
+      if (cpt == 4) {
+        return true;
       }
+    }
     parcours_lig = ligne;
     parcours_col = colonne;
-    while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col >= 0 && parcours_col < largeur && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
-        parcours_lig--;
-        parcours_col++;
-        cpt++;
-        if (cpt == 4) {return true;}
+    while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col >= 0 && parcours_col < largeur
+        && lautreJoueur(convert(getJeton(ligne, colonne)))) {
+      parcours_lig--;
+      parcours_col++;
+      cpt++;
+      if (cpt == 4) {
+        return true;
       }
-    return false;                                                                 
-  } 
-  
-  public boolean victoireColonne(int ligne, int colonne){
+    }
+    return false;
+  }
+
+  public boolean victoireColonne(int ligne, int colonne) {
     int parcours_lig = ligne;
-      int parcours_col = colonne;
-      int cpt = 0;
-      while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col < largeur && parcours_col > 0 && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
-        parcours_lig--;
-        cpt++;
-        if (cpt == 4) {return true;}
-        }
-      parcours_lig = ligne;
-      parcours_col = colonne;
-      while (parcours_lig >= 0 && parcours_lig < hauteur && parcours_col > 0 && parcours_col < largeur && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
-          parcours_lig++;
-          cpt++;
-          if (cpt == 4) {return true;}
-        }
-      return false;                                                                 
-    } 
+    int parcours_col = colonne;
+    int cpt = 0;
+    while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col < largeur && parcours_col >= 0
+        && lautreJoueur(convert(getJeton(ligne, colonne)))) {
+      cpt++;
+      parcours_lig--;
+      System.out.println(getJeton(ligne, colonne));
+      System.out.println(cpt);
+      if (cpt == 4) {
+        return true;
+      }
+    }
+    System.out.println("autre direction");
+    parcours_lig = ligne;
+    parcours_col = colonne;
+    while (parcours_lig >= 0 && parcours_lig < hauteur && parcours_col >= 0 && parcours_col < largeur
+        && lautreJoueur(convert(getJeton(ligne, colonne)))) {
+      parcours_lig++;
+      System.out.println(getJeton(ligne, colonne));
+      System.out.println(cpt);
+      cpt++;
+      if (cpt == 4) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-    public boolean victoireLigne(int ligne, int colonne){
-      int parcours_lig = ligne;
-        int parcours_col = colonne;
-        int cpt = 0;
-        while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col < largeur && parcours_col > 0 && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
-          parcours_col--;
-          cpt++;
-          if (cpt == 4) {return true;}
-          }
-        parcours_lig = ligne;
-        parcours_col = colonne;
-        while (parcours_lig > 0 && parcours_lig < hauteur && parcours_col > 0 && parcours_col < largeur && (!(convert(getJeton(parcours_lig, parcours_col)).equals(joueurActuel)))){
-            parcours_col++;
-            cpt++;
-            if (cpt == 4) {return true;}
-          }
-        return false;                                                                 
-      } 
+  public boolean victoireLigne(int ligne, int colonne) {
+    int parcours_lig = ligne;
+    int parcours_col = colonne;
+    int cpt = 0;
 
+    while (parcours_lig >= 0 && parcours_lig < hauteur && parcours_col < largeur && parcours_col > 0
+        && lautreJoueur(convert(getJeton(ligne, colonne)))) {
+      parcours_col--;
+      cpt++;
+      System.out.println(cpt);
+      if (cpt == 4) {
+        return true;
+      }
+    }
+    parcours_lig = ligne;
+    parcours_col = colonne;
+    while (parcours_lig >= 0 && parcours_lig < hauteur && parcours_col >= 0 && parcours_col < largeur
+        && lautreJoueur(convert(getJeton(ligne, colonne)))) {
+      parcours_col++;
+      cpt++;
+      System.out.println(cpt);
+      if (cpt == 4) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   @Override
-  public String toString(){
+  public String toString() {
     String res = "Plateau P4 \n";
-    for (List<Integer> col : plateau){
+    for (List<Integer> col : plateau) {
       res += col + "\n";
     }
     return res;
   }
-
-
-
 
 }
