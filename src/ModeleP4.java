51d3d352e4fc1211c7c3902c565c5612eed778f7
IckaeEtu <mickael.riego@etu.univ-orleans.fr>
2024-06-17 18:21:50 +0200

    doc finit pour modeleP4

--- src/ModeleP4.java
+++ src/ModeleP4.java
@@ -38,15 +38,28 @@ public class ModeleP4 {
       changerJoueur();
     }
   }
-
+  /**Renvoie si la collone est pleine 
+   * @param colonne l'indice de la colonne 
+   * @return true ou false
+   * */
   public boolean colPleinne(int colonne) {
     return plateau.get(colonne).size() < hauteur;
   }
-
+  /**
+   * renvoi le jeton a l'emplacement ligne,colonne
+   * @param ligne le numéros de la ligne
+   * @param colonne le numéros de la colonne
+   * @return  la valeur du jeton 0,1 joueur 1,2 joueur 2
+   */
   public int getJeton(int ligne, int colonne) {
     return plateau.get(colonne).get(ligne);
   }
 
+  /**
+   * convertis la valeur d'un jeton en un joueur
+   * @param num 0,1,2 un jeton
+   * @return J1 pour 1 et J2 pour 2 sinon NON
+   */
   public String convert(int num) {
     if (num == 1) {
       return "J1";
@@ -55,7 +68,12 @@ public class ModeleP4 {
     }
     return "Non";
   }
-
+  
+  /**
+   * permet de vérifier que le jeton est la valeur voulus, c-à-d 1 si J1 vient jouer 2 sinon
+   * @param joueur J1 ou J2
+   * @return true ou false si c'est la valeur voulut
+   */
   public boolean lautreJoueur(String joueur) {
     if (joueur.equals("J2") && joueurActuel.equals("J1")) {
       return true;
@@ -66,6 +84,9 @@ public class ModeleP4 {
     return false;
   }
 
+  /**
+   * échange la valeur de joueur actuel entre J1 et J2
+   */
   public void changerJoueur() {
     if (joueurActuel.equals("J1")) {
       joueurActuel = "J2";
@@ -73,11 +94,18 @@ public class ModeleP4 {
       joueurActuel = "J1";
     }
   }
-
+  /**
+   * permet de savoir le joueur actuel, a qui c'est le tour
+   * @return le joueur actuel
+   */
   public String getJoueurActuel() {
     return this.joueurActuel;
   }
-
+  /**
+   * Renvoie le première indice de dans la colonne qui est vide
+   * @param colonne indice d'une colonne du plateau
+   * @return l'indice de la première ligne vide
+   */
   public int ligneVide(int colonne) {
     for (int i = 0; i < hauteur; i++) {
       if (plateau.get(colonne).get(i) == 0) {
@@ -86,12 +114,22 @@ public class ModeleP4 {
     }
     return 42;
   }
-
+  /**
+   * test toutes les conditions de victoire
+   * @param ligne coordonée ligne
+   * @param colonne coordonée colonne
+   * @return true ou fales si une condition est validée
+   */
   public boolean victoire(int ligne, int colonne) {
     return victoireDiag1(ligne, colonne) || victoireDiag2(ligne, colonne) || victoireColonne()
         || victoireLigne(ligne, colonne);
   }
-
+  /**
+   * test la victoire sur une des diagonales
+   * @param ligne coordonée ligne
+   * @param colonne coordonée colonne
+   * @return true ou false
+   */
   public boolean victoireDiag1(int ligne, int colonne) {
     int parcours_lig = ligne;
     int parcours_col = colonne;
@@ -118,7 +156,12 @@ public class ModeleP4 {
     }
     return false;
   }
-
+  /**
+   * test la victoire sur l'autre diagonale
+   * @param ligne coordonée ligne
+   * @param colonne coordonée colonne
+   * @return true ou false
+   */
   public boolean victoireDiag2(int ligne, int colonne) {
     int parcours_lig = ligne;
     int parcours_col = colonne;
@@ -145,7 +188,10 @@ public class ModeleP4 {
     }
     return false;
   }
-
+  /**
+   * test la victoire sur la colonne
+  * @return true si il y a 4 pion aligné dans la colonne, false sinon
+   */
   public boolean victoireColonne() {
     int condi=0;
     for (List<Integer> col:plateau){
@@ -162,7 +208,12 @@ public class ModeleP4 {
     }
     return false;
   }
-
+  /**
+   * test la victoire sur la ligne
+   * @param ligne indice de ligne
+   * @param colonne indice de colonne
+   * @return true si il y a 4 pion aligné dans la ligne, false sinon
+   */
   public boolean victoireLigne(int ligne, int colonne) {
     int parcours_lig = ligne;
     int parcours_col = colonne;
@@ -190,7 +241,10 @@ public class ModeleP4 {
     }
     return false;
   }
-
+  /**
+   * permet de vérfier si la grille est pleine
+   * @return true si la grille est remplie, false sinon
+   */
   public boolean grillePleine(){
     for (List<Integer> col:plateau){
       for (Integer val:col){
@@ -200,7 +254,9 @@ public class ModeleP4 {
     return true;
   }
 
-
+  /**
+   * remet tout à l'état de base
+   */
   public void reset(){
     plateau = new ArrayList<>();
     for (int i = 0; i < largeur; i++) {
@@ -211,6 +267,10 @@ public class ModeleP4 {
     }
   }
 
+  /**
+   * permet d'afficher la grille pour les test, la grille doit être tourné de 90°
+   * dans le sens inverses des aiguilles d'une montre
+   */
   @Override
   public String toString() {
     String res = "Plateau P4 \n";
